4d7ceb49ba062f95f6a4df1395533a6163cbf444
Kyle Cheung <kcheu4@uwo.ca>
2019-05-22 19:58:54 -0400

    moved router calls from index to queries

--- server/index.js
+++ server/index.js
@@ -1,26 +1,14 @@
 require('dotenv').config();
-const api_helper = require('./API_helper')
 const express = require('express');
 const request = require('request');
 const app = express();
-const key = process.env.DARK_SKY_SECRET;
 const port = process.env.PORT || 3000;
 const db = require('./queries');
-const darksky = 'https://api.darksky.net/forecast/'
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-})
+// Routes
+app.get('/', db.root)
 
-app.get('/call', (req, res) => {
-    api_helper.make_API_call(`${darksky}${key}/49.9415,-79.5134`)
-        .then(response => {
-            res.json(response)
-        })
-        .catch(error => {
-            res.send(error)
-        })
-})
+app.get('/call', db.call)
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}.`);
--- server/queries.js
+++ server/queries.js
@@ -1,4 +1,40 @@
 require('dotenv').config()
+const api_helper = require('./API_helper');
 const key = process.env.DARK_SKY_SECRET;
+const darksky = 'https://api.darksky.net/forecast/';
 
+var testObject = {
+    "data": []
+};
+
+const format = data => {
+
+    let i;
+    for( i = 0; i <= 7; i++) {
+        testObject.data.push(data.daily.data[i]);
+
+    }
+}
+const root = (req, res) => {
+    res.send('Hello World!');
+}
+
+const call = (req, res) => {
+    api_helper.make_API_call(`${darksky}${key}/49.9415,-79.5134`)
+        .then(response => {
+
+            format(response)
+            res.json(testObject)
+
+            //res.json(response)
+        })
+        .catch(error => {
+            res.send(error)
+        })
+}
+
+module.exports = {
+    root,
+    call,
+}
 
