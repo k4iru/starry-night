7b0b89ef88fe5682190c635408c9701ff9e84c55
Kyle Cheung <kcheu4@uwo.ca>
2019-06-13 02:08:06 -0400

    production

---
+++ client/build/precache-manifest.91db1b49809438a5a0ade845301ffb41.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b107551786a77ba4a5dce5b3adc3293",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72e279315b92f3fe886a",
+    "url": "/static/css/main.7017d7f9.chunk.css"
+  },
+  {
+    "revision": "bd884e16472d91849814",
+    "url": "/static/js/2.3ea4e5cf.chunk.js"
+  },
+  {
+    "revision": "72e279315b92f3fe886a",
+    "url": "/static/js/main.ed2d22ed.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "99448c54ca59ceb88ae529db4aa8ac23",
+    "url": "/static/media/second.99448c54.jpg"
+  },
+  {
+    "revision": "8a78d3487696284ee5cd7fbc08177be4",
+    "url": "/static/media/stars.8a78d348.jpg"
+  }
+]);
