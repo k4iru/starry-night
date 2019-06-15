c7ea735f864bf4c85c959efbdeb39fc27a353595
Kyle Cheung <kcheu4@uwo.ca>
2019-06-15 19:02:12 -0400

    fixes

--- server.js
+++ server.js
@@ -18,10 +18,10 @@ app.get('/', db.root)
 app.get('/call', db.call)
 
 if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.use(express.static(path.join(__dirname, 'client/build')));
 
     app.get('*', function(req, res) {
-        res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
     });
 }
 
